207b8d6977e04bb0bd3ce3d97a0b6c54d45dde04
theenash <theenashmaran@gmail.com>
2025-07-22 14:08:01 +0530

    Time: 83 ms (12.45%), Space: 125.6 MB (55.55%) - LeetHub

--- 3936-split-array-by-prime-indices/3936-split-array-by-prime-indices.cpp
+++ 3936-split-array-by-prime-indices/3936-split-array-by-prime-indices.cpp
@@ -14,14 +14,11 @@ public:
 
         long long sumA = 0, sumB = 0;
 
-        vector<long long> a,b;
         for(int i=0;i<nums.size();i++){
             if(isPrime(i)){
-                a.push_back(nums[i]);
                 sumA += nums[i];
             }
             else{
-                b.push_back(nums[i]);
                 sumB += nums[i];
             }
         }

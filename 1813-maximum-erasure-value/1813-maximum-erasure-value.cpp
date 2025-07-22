d492155316201ec5e0e1e14b4aad792c5dbe5078
theenash <theenashmaran@gmail.com>
2025-07-22 12:35:52 +0530

    Time: 1669 ms (5.01%), Space: 160.3 MB (5.65%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -3,15 +3,11 @@ public:
     int maximumUniqueSubarray(vector<int>& nums) {
         map<int,int> mapp;
         int n = nums.size();
-
         int i = 0, j = 0;
-
         int maxCount = 0, maxSum = 0;
         int sum = 0;
         int start,end;
-
         while(i<n && j<n){
-
             while(mapp.count(nums[j])){
                 maxCount = max(maxCount, j - i - 1);
                 sum = 0;
@@ -19,12 +15,10 @@ public:
                     sum += nums[a];
                 }
                 maxSum = max(maxSum , sum);
-
                 mapp[nums[i]]--;
                 if(mapp[nums[i]] == 0) mapp.erase(nums[i]);
                 i++;
             }
-
             mapp[nums[j]]++;
             j++;
         }

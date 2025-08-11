0f2a62b9e63225fd6ad5bbf27bf03522a6cb2296
theenash <theenashmaran@gmail.com>
2025-08-11 11:56:52 +0530

    Time: 0 ms (100%), Space: 33.9 MB (6.67%) - LeetHub

--- 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>> solve(vector<vector<int>> &grid, int x, int y, int k){
+        int m = grid.size(), n = grid[0].size();
+
+        int l = x, r = x + k - 1;
+
+        while(l < r){
+            for(int i=y;i<=y+k-1;i++){
+                swap(grid[l][i], grid[r][i]);
+            }
+            l++; r--;
+        }
+        
+        return grid;
+    }
+
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+
+
+        return solve(grid,x,y,k);
+
         int m = grid.size(), n = grid[0].size();
 
         int xx = x, yy = y;
         vector<vector<int>> mat;
 
+// ...................finding the Square submatrix and storing as new matrix..............
         for(int i=0;i<m;i++){
             int flag = 0;
             vector<int> temp;
@@ -37,12 +57,14 @@ public:
         int mm = mat.size();
         int nn = mat[0].size();
 
+// ...............................swapping the submatrix................
         for(int i=0;i<mm/2;i++){
             for(int j=0;j<nn;j++){
                 swap(mat[i][j] , mat[mm - i - 1][j]);
             }
         }
 
+// .............................storing the swapped submatrix to the original matrix itself
         xx = x, yy = y;
         int matr = 0, matc = 0;
         for(int i=0;i<m;i++){
@@ -69,6 +91,22 @@ public:
             if(xx > k + x - 1) break;
         }
 
+        /*
+        t.c :
+        0(m * n) - for traversing the matrix to find the submatrix
+        O(mm * nn) - for swapping in th submatrix (mm == nn == k)
+        O(m * n) - for modifying the swapped submatrx in original matrix
+
+        overall :: 2 * O(m * n) + O(k * k)
+
+        space complexity :
+        extra space for storing submatrix
+
+        s.c = O(k*k)
+        */
+
         return grid;
+
+
     }
 };

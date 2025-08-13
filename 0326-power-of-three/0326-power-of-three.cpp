974b4a537f2cb2e1a9ab3c1ee8e4caf95c699c25
theenash <theenashmaran@gmail.com>
2025-08-13 09:15:17 +0530

    Time: 60 ms (5.68%), Space: 8.9 MB (76.82%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-
+/*
     bool func(int n, long long pow){
         if(pow == n) return true;
         if(pow > n) return false;
         return func(n, pow * 3); 
     }
+*/
+    bool func(double n){
+        if(n == 1) return true;
+        if(n == 0) return false;
+        return func(n/3);
+    }
 
     bool isPowerOfThree(int n) {
         long long pow = 1;
-        return func(n, pow);
+        return func(n);
     }
 };

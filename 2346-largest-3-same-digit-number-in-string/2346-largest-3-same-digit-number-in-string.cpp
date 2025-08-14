689d44902dd35a568c3d460f99aa362b2b13bb85
theenash <theenashmaran@gmail.com>
2025-08-14 13:31:14 +0530

    Time: 4 ms (5.98%), Space: 8.9 MB (77.37%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -3,55 +3,18 @@ public:
     string largestGoodInteger(string num) {
         int n = num.size();
 
-        vector<string> numbers;
+        string s = "", res = "";
+        for(int i=0;i<num.size()-2;i++){
+            int j = i+1, k = i+2;
 
-        string s = "";
-        int cnt = 0;
-        int flag = 0;
-        for(int i=0;i<n-1;i++){
-            if(num[i] == num[i+1] && flag == 0){
+            if(num[i] == num[j] && num[j] == num[k]){
                 s += num[i];
-                s += num[i+1];
-                cnt+=2;
-                flag = 1;
-            }
-            else if(flag && num[i] == num[i+1] && cnt < 3){
-                s += num[i+1];
-                cnt++;
-            }
-            else if(flag && cnt == 3){
-                numbers.push_back(s);
-                cnt = 0;
+                s += num[j]; 
+                s += num[k];
+                res = max(res, s);
                 s = "";
-                flag = 0;
-                i--;
-            }
-            else if(flag && num[i] != num[i+1] && cnt < 3){
-                flag = 0;
-                s = "";
-                cnt = 0;
             }
         }
-        if(flag && cnt == 3){
-            numbers.push_back(s);
-        }
-
-        for(auto i: numbers) cout<<i<<" ";
-        cout<<endl;
-
-
-        int maxi = INT_MIN;
-        int ind = -1;
-        for(int i=0;i<numbers.size();i++){
-            if(numbers[i].size() == 3){
-                int num = stoi(numbers[i]);
-                if(maxi < num){
-                    maxi = num;
-                    ind = i;
-                }
-            }
-        }
-        if(ind != -1) return numbers[ind];
-        return "";
+        return res;
     }
 };

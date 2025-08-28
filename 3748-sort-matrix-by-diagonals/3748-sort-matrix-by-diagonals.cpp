d8a08d3d51d6af545ae9f4e45936a3b1d6ad0352
theenash <theenashmaran@gmail.com>
2025-08-28 21:56:46 +0530

    Time: 97 ms (5.76%), Space: 44 MB (60.08%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int flag = 0;
+
+        // upper triangle:
+        for(int col = 1;col < n; col++){
+            int times = n - col;
+            vector<int> temp;
+            int row = 0;
+            int j = col; // column
+            while(times--){
+                temp.push_back(grid[row++][j++]);
+            }
+            sort(temp.begin(), temp.end());
+            
+            times = n - col;
+            row = 0; j = col;
+            int ind = 0;
+            while(times--){
+                grid[row++][j++] = temp[ind++];
+            }   
+        }
+
+        // upper triangle with diagonal:
+        for(int row = 0;row < n; row++){
+            int times = n - row;
+            vector<int> temp;
+            int col = 0;
+            int r = row; // column
+            while(times--){
+                temp.push_back(grid[r++][col++]);
+            }
+            sort(temp.begin(), temp.end(), greater<int>());
+            
+            times = n - row;
+            r = row; col = 0;
+            int ind = 0;
+            while(times--){
+                grid[r++][col++] = temp[ind++];
+            }   
+        }
+
+        for(auto i: grid){
+            for(auto j: i){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+
+        return grid;
+
+    }
+};

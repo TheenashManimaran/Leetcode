7e73b5bac574a52b64d5c083e47e02b0e36a60c5
theenash <theenashmaran@gmail.com>
2025-08-31 11:06:48 +0530

    Time: 6 ms (3.13%), Space: 8.7 MB (39.52%) - LeetHub

---
+++ 2284-smallest-value-of-the-rearranged-number/2284-smallest-value-of-the-rearranged-number.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    long long smallestNumber(long long num) {
+        int flag = 1;
+
+        if(num < 0) {flag = -1; num = -1 * num;}
+        
+        vector<int> nums;
+
+        int countzero = 0;
+        while(num!=0){
+            if(num % 10 == 0) countzero++;
+            else nums.push_back(num % 10);
+            num /= 10;
+        }
+        long long number = 0;
+        if(flag == 1){
+            cout<<"flag :"<<flag <<endl;
+            sort(nums.begin(), nums.end());
+            if(countzero > 0){
+                while(countzero--){
+                    nums.insert(nums.begin() + 1, 0);
+                }
+            }
+            for(int i: nums) cout<<i<<" ";
+            cout<<endl;
+            long long places = 1;
+            
+            for(int i=nums.size() -1; i>=0 ;i--){
+                number += nums[i] * places;
+                places *= 10;
+            }
+        }
+        else{
+            sort(nums.begin(), nums.end(), greater<int>());
+            if(countzero > 0){
+                while(countzero--){
+                    nums.push_back(0);
+                }
+            }
+
+            for(int i: nums) cout<<i<<" ";
+            cout<<endl;
+            long long places = 1;
+            
+            for(int i=nums.size() -1; i>=0 ;i--){
+                number += nums[i] * places;
+                places *= 10;
+            }
+
+            number = flag * number;
+
+        }
+        return number;
+    }
+};

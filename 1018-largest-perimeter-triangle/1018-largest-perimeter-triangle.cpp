3cbd7b04dd28a5f9a81eacf06eead1354b65758f
theenash <theenashmaran@gmail.com>
2025-10-08 22:32:20 +0530

    Time: 4 ms (64.14%), Space: 25.6 MB (47.79%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -4,12 +4,12 @@ public:
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
-        int maxi = 0;
-        for(int i=0;i<=n-3;i++){
-            if(nums[i] + nums[i+1] > nums[i+2]){
-                maxi = max(maxi, nums[i] + nums[i+1] + nums[i+2]);
+        // checking from larger to smaller
+        for(int i=n-1;i>=2;i--){
+            if(nums[i-2] + nums[i-1] > nums[i]){
+                return nums[i] + nums[i-1] + nums[i-2];
             }
         }
-        return maxi;
+        return 0;
     }
 };

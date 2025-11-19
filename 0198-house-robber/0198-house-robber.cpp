d3bc5d3340cdc9ca87e0d51be3a9a9f46dd9261f
theenash <theenashmaran@gmail.com>
2025-11-19 14:38:40 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // Memoization
+    // Tabulation
 
     int solve(int ind, vector<int> &nums, vector<int> &dp){
         if(ind == 0) return nums[ind];
@@ -18,9 +18,20 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> dp(n, -1);
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
 
-        return solve(n-1, nums, dp);
+        for(int i=1;i<n;i++){
+            
+            int take = nums[i];
+            if(i - 2 >= 0) take += dp[i-2];
+
+            int notTake = 0 + dp[i-1];
+
+            dp[i] = max(take, notTake);
+        }
+
+        return dp[n-1];
 
     }
 };

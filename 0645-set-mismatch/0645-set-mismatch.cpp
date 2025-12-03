e63aeee97d1216a6fdb96e160557568ed80bc8b8
theenash <theenashmaran@gmail.com>
2025-12-04 00:11:29 +0530

    Time: 0 ms (100%), Space: 26.4 MB (33.48%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+
+        int n = nums.size();
+        // to check
+        vector<int> arr(n);
+
+        map<int,int> mapp;
+
+        vector<int> ans(2);
+        int found = 0;
+        for(int i=0;i<n;i++){
+            if(arr[nums[i]-1] >= 1){
+                ans[0] = nums[i];
+            }
+            else arr[nums[i]-1] += 1;
+        }
+
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0){
+                ans[1] = i+1;
+                break;
+            }
+        }
+
+        return ans;
+
+        
+
+           
+    }
+};

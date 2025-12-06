5b65ec5908f10e18ac5ad5395392bfc8623a472c
theenash <theenashmaran@gmail.com>
2025-12-06 07:51:35 +0530

    Time: 0 ms (100%), Space: 9.4 MB (49.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -15,11 +15,34 @@ public:
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+//.....................................Tabulation
+        dp[0][0] = 1;
 
-        int ans = solve(m-1,n-1, dp);
-        return ans;
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
 
-        
+                if(r == 0 && c == 0) continue;
+
+                int left = 0, up = 0;
+
+                if(r > 0) up = dp[r-1][c];
+
+                if(c > 0) left = dp[r][c-1];
+
+                dp[r][c] = up + left;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
+
+/*
+steps to convert memoization to tabultaion
+
+1. Base case
+2. express the states as for loop eg. r - for i , c - inner for j
+3. copy the recurrence
+
+*/

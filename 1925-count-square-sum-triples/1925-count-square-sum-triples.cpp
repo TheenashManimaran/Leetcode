db3389a3903f5f6024944430aff556d6021f7784
theenash <theenashmaran@gmail.com>
2025-12-08 19:56:53 +0530

    Time: 215 ms (21.68%), Space: 7.8 MB (55.75%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        
+        int count = 0;
+        for(int i = 3; i<n;i++){
+            for(int j=3;j<n;j++){
+                for(int k = 5;k<=n;k++){
+                    if((i * i) + (j*j) == (k*k)) {
+                        count+=1;
+                        //cout<<i<<" "<<j<<" "<<k<<endl;
+                    }
+                    
+                }
+            }
+        }
+
+        return count;
+    }
+};

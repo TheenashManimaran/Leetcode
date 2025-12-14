bd0495c542ab526b8ba32a47e2998e9a8edbed14
theenash <theenashmaran@gmail.com>
2025-12-14 19:48:18 +0530

    Time: 3 ms (34.51%), Space: 15.1 MB (38.23%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -16,26 +16,29 @@ public:
         return dp[r][c] = min(up, min(upleft, upright));
     }
 
-    int tabulate(int n, vector<vector<int>> &matrix, vector<vector<int>> &dp){
+    int spaceOpt(int n, vector<vector<int>> &matrix){
+        vector<int> prev(n), cur(n);
+
         for(int i = 0;i<n;i++) {
-            dp[0][i] = matrix[0][i];
+            prev[i] = matrix[0][i];
         }
 
         for(int r = 1; r < n; r++){
             for(int c = 0; c < n; c++){
                 int up = 1e9, upleft = 1e9, upright = 1e9;
 
-                if(c > 0) upleft = matrix[r][c] + dp[r-1][c-1];
-                if(c < n-1) upright = matrix[r][c] + dp[r-1][c+1];
-                up = matrix[r][c] + dp[r-1][c];
+                if(c > 0) upleft = matrix[r][c] + prev[c-1];
+                if(c < n-1) upright = matrix[r][c] + prev[c+1];
+                up = matrix[r][c] + prev[c];
 
-                dp[r][c] = min(up, min(upleft, upright));
+                cur[c] = min(up, min(upleft, upright));
             }
+            prev = cur;
         }
 
         int mini = INT_MAX;
         for(int i=0;i<n;i++){
-            mini = min(mini, dp[n-1][i]);
+            mini = min(mini, prev[i]);
         }
 
         return mini;
@@ -44,16 +47,19 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         
-        // int mini = INT_MAX;
-        // for(int i=0;i<n;i++){
-        //     vector<vector<int>> dp(n, vector<int>(n, -1));
-        //     mini = min(solve(n-1, , matrix, n, dp), mini);
-        // }
+        /*   ||| DP - MEMOIZATION |||
 
-        // return mini;
+        int mini = INT_MAX;
+        for(int i=0;i<n;i++){
+            vector<vector<int>> dp(n, vector<int>(n, -1));
+            mini = min(solve(n-1, i, matrix, n, dp), mini);
+        }
+
+        return mini;
+        */
 
         vector<vector<int>> dp(n, vector<int>(n, 0));
-        int ans = tabulate(n, matrix, dp);
+        int ans = spaceOpt(n, matrix);
 
         return ans;
     }

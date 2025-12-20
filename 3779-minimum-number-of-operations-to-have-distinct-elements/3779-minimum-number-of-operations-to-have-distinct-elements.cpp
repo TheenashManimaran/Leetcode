20eba7413fed2a0548a3136c21008188079026a0
theenash <theenashmaran@gmail.com>
2025-12-21 02:47:45 +0530

    Time: 90 ms (64.29%), Space: 298.5 MB (7.14%) - LeetHub

--- 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        unordered_map<int,int> mapp;
+        vector<int> hash(1e5);
         int n = nums.size();
 
         int cnt = 0;
         for(int i=n-1;i>=0;i--){
-            if(mapp[nums[i]]) break;
-            mapp[nums[i]]++;
+            if(hash[nums[i]-1] >= 1) break;
+            hash[nums[i]-1]++;
             cnt++;
         }
         cout<<n-cnt<<endl;
